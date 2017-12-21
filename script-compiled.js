7c100837d66ee291cda85d039b61a1586b8e9940
benice195 <szczyrbaw@gmail.com>
2017-12-21 16:24:16 +0100

    cwiczenia z ES6

--- script-compiled.js
+++ script-compiled.js
@@ -1,6 +1,52 @@
-'use strict';
+"use strict";
 
-var sayHello = function sayHello() {
-  return alert('Hello world!');
+//const sayHello = () => alert('Hello world!');
+//sayHello();
+
+//Zadanie 1
+var stringOne = "Hello";
+var stringTwo = "World!";
+
+console.log(stringOne + " " + stringTwo);
+
+//Zadanie 2
+
+var multiply = function multiply() {
+  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return console.log(x + y);
 };
-sayHello();
+multiply(2, 5);
+multiply(6, 6);
+multiply(2);
+
+//Zadanie 3
+
+var average = function average(args) {
+  return args.reduce(function (a, b) {
+    return a + b;
+  }, 0) / args.length;
+};
+
+var res1 = average([1, 3, 2]);
+var res2 = average([10, 30, 55, 72]);
+console.log(res1);
+console.log(res2);
+
+//Zadanie 4
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+console.log(average([].concat(grades)));
+
+//Zadanie 5
+
+var someArr = [1, 4, 'Iwona', false, 'Nowak'];
+var a = someArr[0],
+    b = someArr[1],
+    c = someArr[2],
+    d = someArr[3],
+    e = someArr[4];
+
+
+console.log(c, e);
--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-const sayHello = () => alert('Hello world!');
-sayHello();
+//const sayHello = () => alert('Hello world!');
+//sayHello();
+
+//Zadanie 1
+const stringOne = "Hello";
+const stringTwo = "World!";
+
+console.log(`${stringOne} ${stringTwo}`);
+
+//Zadanie 2
+
+const multiply = (x = 1, y = 1) => {return console.log(x + y)};
+multiply(2, 5);
+multiply(6, 6);
+multiply(2);
+
+//Zadanie 3
+
+const average = args => args.reduce((a, b) => a + b, 0) / args.length;
+
+const res1 = average([1, 3, 2]);
+const res2 = average([10, 30, 55, 72]);
+console.log(res1);
+console.log(res2);
+
+//Zadanie 4
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+console.log(average([...grades]));
+
+//Zadanie 5
+
+const someArr = [1, 4, 'Iwona', false, 'Nowak'];
+const [a, b, c, d, e] = someArr;
+
+console.log(c, e);
